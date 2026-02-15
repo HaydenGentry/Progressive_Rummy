dc80d58a4f473a46bcad40c574f68fd9d1cb866e
HaydenGentry <120219452+HaydenGentry@users.noreply.github.com>
2026-02-15 18:05:33 -0500

    v1.0.7 (Proj. Structure and Split Lay Down)
    Implemented split_can_lay_down boolean, cleaned up lay_down_contracts logic, and added an array for single_cards to make the closeness cards and number easier to implement later. 
    Changed the file structure to make the code more modular as the project begins to grow
    Consider modularizing the current large function that I have been working on into three separate functions:
    One that sorts that hand, one that determines if laying down is possible, and one that determines the cards to lay down if possible

---
+++ first_round_logic.py
@@ -0,0 +1,382 @@
+# v1.0.7
+# v1 = number of files (only file is for game logic right now)
+# v1.0 = number of rounds completed (working on first round, not complete yet)
+# v1.0.2 = number of commits since previous round complete milestone
+
+import random
+import math
+from shlex import split
+
+two_full_decks = ["A-S", "2-S", "3-S", "4-S", "5-S", "6-S", "7-S", "8-S", "9-S", "10-S", "J-S", "Q-S", "K-S",
+                  "A-S", "2-S", "3-S", "4-S", "5-S", "6-S", "7-S", "8-S", "9-S", "10-S", "J-S", "Q-S", "K-S",
+                  "A-D", "2-D", "3-D", "4-D", "5-D", "6-D", "7-D", "8-D", "9-D", "10-D", "J-D", "Q-D", "K-D",
+                  "A-D", "2-D", "3-D", "4-D", "5-D", "6-D", "7-D", "8-D", "9-D", "10-D", "J-D", "Q-D", "K-D",
+                  "A-C", "2-C", "3-C", "4-C", "5-C", "6-C", "7-C", "8-C", "9-C", "10-C", "J-C", "Q-C", "K-C",
+                  "A-C", "2-C", "3-C", "4-C", "5-C", "6-C", "7-C", "8-C", "9-C", "10-C", "J-C", "Q-C", "K-C",
+                  "A-H", "2-H", "3-H", "4-H", "5-H", "6-H", "7-H", "8-H", "9-H", "10-H", "J-H", "Q-H", "K-H",
+                  "A-H", "2-H", "3-H", "4-H", "5-H", "6-H", "7-H", "8-H", "9-H", "10-H", "J-H", "Q-H", "K-H",
+                  "JOKER", "JOKER", "JOKER", "JOKER"]
+players_hands = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [],
+                 6: [], 7: [], 8: [], 9:[], 10: []} # debugging. Only keep 0-5 for actual gameplay
+
+# Progressive Rummy Rounds:
+# Round 1. 2 sets
+# Round 2. 1 set and 1 run
+# Round 3. 2 runs
+# Round 4. 3 sets
+# Round 5. 2 sets, 1 run, and no discard
+
+
+# Every player is dealt 9 cards at the beginning of each round
+def shuffle_and_deal(decks):
+    random.shuffle(decks)
+    # debugging
+    print(decks)
+    for player in range(num_players):
+        for card in range(9):
+            players_hands[player].append(decks[player + (num_players * card)])
+    # debugging
+        print("Player " + str(player + 1) + ": " + str(players_hands[player]))
+    print("First Card: " + str(decks[num_players * 9]))
+
+
+# Return True if value is a number card. Return False if value is not a number card
+def a_number_card(card):
+    # Joker
+    if len(card) == 5:
+        return False
+    else:
+        # Number card
+        try:
+            int(card[0])
+            return True
+        # Face card
+        except ValueError:
+            return False
+
+
+# Return the value and suit of a card. If a joker, only return the value
+def value_and_suit(card):
+    sep = "-"
+    value_arr, suit_arr = [], []
+    # Joker return arr of length 1 (no suit)
+    if len(card) == 5:
+        value_arr.append("JOKER")
+    else:
+        value, dash, suit = card.partition(sep)
+        # Number card returns int value and suit arr of length 2
+        if a_number_card(value):
+            value_arr.append(int(value))
+            suit_arr.append(suit)
+        # Face card returns face value and suit arr of length 2
+        else:
+            value_arr.append(value)
+            suit_arr.append(suit)
+    # debugging
+    # print(value_arr + suit_arr)
+    return value_arr + suit_arr
+
+
+# Return the count of jokers in a players hand
+def joker_count(p_hand):
+    jokers = 0
+    for card in p_hand:
+        if value_and_suit(card)[0] == "JOKER":
+            jokers += 1
+    # debugging
+    # print("1 jokers: " + str(p_hand) + str(joker_count))
+    return jokers
+
+
+# Return the players hand sorted only by the value of the cards in ascending order
+def sort_values(p_hand):
+    # 1. Naive sort of all players hands
+    p_hand.sort()
+    # debugging
+    # print("1 pre: " + str(p_hand))
+    # 2. Split hand into number and face cards
+    number_cards, face_cards = [], []
+    for card in p_hand:
+        if a_number_card(card):
+            number_cards.append(card)
+        else:
+            face_cards.append(card)
+    # debugging
+    # print("2 nums: " + str(number_cards))
+    # print("2 faces: " + str(face_cards))
+    # 3. Re-sort number cards with 10 in the back (currently in front)
+    i = 0
+    for card in number_cards:
+        if value_and_suit(card)[0] == 10:
+            i += 1
+    ten_cards = number_cards[:i]
+    number_cards = number_cards[i:] + ten_cards
+    # debugging
+    # print("3 nums: " + str(number_cards))
+    # 4. Re-sort face cards. Use switch case J-Q-K-A-JOK (push A and JOK to the back)
+    j_cards, q_cards, k_cards, a_cards, jok_cards = [], [], [], [], []
+    for card in face_cards:
+        match value_and_suit(card)[0]:
+            case "J":
+                j_cards.append(card)
+            case "Q":
+                q_cards.append(card)
+            case "K":
+                k_cards.append(card)
+            case "A":
+                a_cards.append(card)
+            case "JOKER":
+                jok_cards.append(card)
+    face_cards = j_cards + q_cards + k_cards + a_cards + jok_cards
+    # debugging
+    # print("4 faces: " + str(face_cards))
+    # 5. Combine lists and print results
+    p_hand = number_cards + face_cards
+    # debugging
+    # print("5 post: " + str(p_hand))
+    return p_hand
+
+
+# H-C-D-S
+# Sort into suits
+def sort_suits(p_hand):
+    h_suits, c_suits, d_suits, s_suits, jok_cards = [],  [],  [], [], []
+    value_sorted = sort_values(p_hand)
+    # debugging
+    # print("Value sorted: " + str(value_sorted))
+    for card in value_sorted:
+        if len(value_and_suit(card)) == 1:
+            jok_cards.append(card)
+        else:
+            match value_and_suit(card)[-1]:
+                case "H":
+                    h_suits.append(card)
+                case "C":
+                    c_suits.append(card)
+                case "D":
+                    d_suits.append(card)
+                case "S":
+                    s_suits.append(card)
+    p_hand = h_suits + c_suits + d_suits + s_suits + jok_cards
+    # debugging
+    print("Suits: " + str(p_hand))
+
+
+# Third order sort
+# Split hand into non/pairs and sets, and pairs and sets
+# Recombine with non/pairs and sets in descending order then pairs and sets in ascending order
+def first_round_two_sets(p_hand):
+    # 1. Sort values in hand
+    value_sorted = sort_values(p_hand)
+    # debugging
+    # print("1. Sort Values: " + str(value_sorted))
+    # 2. Count JOK in hand
+    jokers = joker_count(value_sorted)
+    # debugging
+    # print("2. Jokers: " + str(p_hand) + str(jokers))
+    # 3. Count pairs and sets in hand
+    value_dict = {2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [],
+                  "J": [], "Q": [], "K": [], "A": [], "JOKER": []}
+    value_dict_lens = {2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0,
+                       "J": 0, "Q": 0, "K": 0, "A": 0, "JOKER": 0}
+    pairs = []
+    pair_count = 0
+    sets = []
+    set_count = 0
+    # 3. Put cards in a dictionary of lists for each value as a key
+    for card in value_sorted:
+        value_dict[value_and_suit(card)[0]].append(card)
+    # debugging
+    # print("3. Dict: " + str(value_dict))
+    # 4. Count pairs/sets and record cards in pairs/sets
+    for key in value_dict:
+        value_dict_lens[key] = len(value_dict[key])
+        if (len(value_dict[key]) == 2) and (key != "JOKER"):
+            pairs.append(key)
+            pair_count += 1
+        if (len(value_dict[key]) >= 3) and (key != "JOKER"):
+            sets.append(key)
+            set_count += 1
+    # debugging
+    # print("4. Value len: " + str(value_dict_lens))
+    # print(f"5. Pairs- {pair_count}: {pairs}, Sets- {set_count}: {sets}, Jokers- {jokers}")
+    # Order of hand
+    # 5. Non pairs/sets in descending order + pairs in ascending order + sets in ascending order + jokers
+    sorted_hand = []
+    single_cards = []
+    for key in reversed(value_dict):
+        if (value_dict_lens[key] == 1) and (key != "JOKER"):
+            single_cards.append(value_dict[key])
+    flat_single_cards = [element for sublist in single_cards for element in sublist]
+    sorted_hand.append(flat_single_cards)
+    # debugging
+    # print(f"6a. No pairs/sets/jokers: {flat_single_cards}, {sorted_hand}")
+    if pair_count >= 1:
+        for card in pairs:
+            sorted_hand.append(value_dict[card])
+    # debugging
+    # print("6b. Add pairs: " + str(sorted_hand))
+    if set_count >= 1:
+        for card in sets:
+            sorted_hand.append(value_dict[card])
+    # print("6c. Add sets: " + str(sorted_hand))
+    if jokers >= 1:
+        sorted_hand.append(value_dict["JOKER"])
+    # print("6d. Add jokers: " + str(sorted_hand))
+    # Turn a list of lists into one list
+    flat_sorted_hand = [element for sublist in sorted_hand for element in sublist]
+    # debugging
+    # print("6. Sort no joker placement: " + str(flat_sorted_hand))
+
+    # Lay Down Possibilities
+
+    # LD-NP = Lay Down Not Possible (0), LD-P = Lay Down Possible(1+)
+    # O-LD = Opponent Lay Downs (True/False), PO-LD = Play on Opponent Lay Downs
+    # O-LD_lists = Lists of other contracts that have been laid down
+    # BOL = Back of list
+    # Closeness factor -  # of cards away from LD-P and which card(s) would be needed to achieve it
+    # Close_1 = pairs, 1 card away from a set. Close_2 = singles, 2 cards away from a set
+    # Card-Value gap ??? take a slight loss in value of lay down value if more cards can be laid down
+    # Ex. 5-5-5-5-5 > 9-9-9. I would guess right now the card gap should be >= 2 and value-loss < 5
+
+    # If LD-P > 1, check all possible ways to find the most valuable move
+    # If O-LD = True, check if most valuable move could be improved with PO-LD
+
+    # Scenario 3: 1 set/2+ pairs/2 JOK/O-LD = True ???
+    # O-LD_lists = [[J-J-J], [Q-Q-Q]]
+    # 4-4, 7-7, J-J, Q-Q, JOK: buy 4 from discard and draw JOK
+    # 4-4-4, 7-7, J-J, Q-Q, JOK, JOK: on turn draw 4
+    # 4-4-4-4, 7-7, J-J, Q-Q, JOK, JOK:  best move is LD: 4-4-4-4-JOK, 7-7-JOK + PO-LD: J-J, Q-Q
+    # (1 set/1 pair/2 JOK + PO-LD > 2 pairs/2 JOK).
+
+    # TODO: Need to consider four of a kind or greater (7-7-7-7-+) as a optional pairs/sets if LD-P = 0
+    # Consider max length in value
+    # 0 Jokers, set_count choose 2
+    # 1 Joker, set count choose 2 + set_count * pair_count
+    # 2 or more Jokers, set_count choose 2 + set_count * pair_count + pair_count choose 2
+    lay_down_possibilities = 0
+    if jokers == 0:
+        lay_down_possibilities = math.comb(set_count, 2)
+    if jokers == 1:
+        lay_down_possibilities = math.comb(set_count, 2) + (set_count * pair_count)
+    if jokers >= 2:
+        lay_down_possibilities = math.comb(set_count, 2) + (set_count * pair_count) + math.comb(pair_count, 2)
+
+    # If lay_down_possibilities = 0, can_lay_down = False
+    # Else, can_lay_down = True
+    if lay_down_possibilities == 0:
+        can_lay_down = False
+    else:
+        can_lay_down = True
+
+    # If cannot lay down normally, check if there is a set and if it is 4 cards or more
+    # Based on number of jokers, determine if splitting the set could allow to laydown
+    split_can_lay_down = False
+    if not can_lay_down:
+        if set_count == 1:
+            if jokers == 0:
+                if value_dict_lens[sets[0]] >= 6:
+                    split_can_lay_down = True
+            if jokers == 1:
+                if value_dict_lens[sets[0]] >= 5:
+                    split_can_lay_down = True
+            if jokers >= 2:
+                if value_dict_lens[sets[0]] >= 4:
+                    split_can_lay_down = True
+                    
+    # If there is only one way to lay down the cards in the hand
+    # Lay down the contracts with the JOKs in their legal and valid positions
+    lay_down_contracts = []
+    if can_lay_down and lay_down_possibilities == 1:
+        if jokers == 0:
+            for card in sets:
+                lay_down_contracts.append(value_dict[card])
+        # If one JOK, end of the last set if two sets, or end of the pair if set/pair
+        if jokers == 1:
+            if set_count == 2:
+                for card in sets:
+                    lay_down_contracts.append(value_dict[card])
+                lay_down_contracts.append(['JOKER'])
+            else:
+                for card in pairs:
+                    lay_down_contracts.append(value_dict[card])
+                lay_down_contracts.append(['JOKER'])
+                for card in sets:
+                    lay_down_contracts.append(value_dict[card])
+        # If two JOKs, end of each set if two sets, end of set and pair if set/pair, or end of each pair if two pairs
+        # If three JOKs, same logic as if two JOKs, putting the third JOK in front of the first contract
+        # If four JOKs, same logic as if two JOKs, putting a JOK in front of each contract
+        if jokers >= 2:
+            if set_count == 2:
+                if jokers == 3:
+                    lay_down_contracts.append(['JOKER'])
+                for card in sets:
+                    if jokers == 4:
+                        lay_down_contracts.append(['JOKER'])
+                    lay_down_contracts.append(value_dict[card])
+                    lay_down_contracts.append(['JOKER'])
+            elif set_count == 1:
+                if jokers == 3:
+                    lay_down_contracts.append(['JOKER'])
+                for card in pairs:
+                    if jokers == 4:
+                        lay_down_contracts.append(['JOKER'])
+                    lay_down_contracts.append(value_dict[card])
+                    lay_down_contracts.append(['JOKER'])
+                for card in sets:
+                    if jokers == 4:
+                        lay_down_contracts.append(['JOKER'])
+                    lay_down_contracts.append(value_dict[card])
+                    lay_down_contracts.append(['JOKER'])
+            else:
+                if jokers == 3:
+                    lay_down_contracts.append(['JOKER'])
+                for card in pairs:
+                    if jokers == 4:
+                        lay_down_contracts.append(['JOKER'])
+                    lay_down_contracts.append(value_dict[card])
+                    lay_down_contracts.append(['JOKER'])
+
+    flat_lay_down_contracts = [element for sublist in lay_down_contracts for element in sublist]
+
+    # debugging
+    # print(f"7. LD-P: {flat_sorted_hand}, {can_lay_down}, {split_can_lay_down}, "
+    #       f"{lay_down_possibilities}, {flat_lay_down_contracts}")
+    return flat_sorted_hand, can_lay_down, split_can_lay_down, lay_down_possibilities, flat_lay_down_contracts
+
+
+# Start point
+num_players = 10
+shuffle_and_deal(two_full_decks)
+for player, hand in players_hands.items():
+    if player == num_players:
+        break
+    # debugging
+    print(f"First sort - Player {player + 1}: {first_round_two_sets(hand)}")
+
+# debugging
+test_hand1 = ['9-D', '7-D', '9-H', '5-D', '8-C', '5-S', '3-H', 'JOKER', "10-D", "9-S"]
+test_hand2 = ['Q-H', 'A-S', 'JOKER', '4-D', 'JOKER', 'K-H', '3-S', 'K-D', '5-H']
+test_hand3 = ['8-H', '8-H', '9-H', '9-H', '10-H', '10-H', 'J-S', 'J-H', 'Q-H', 'Q-H', 'JOKER', 'JOKER']
+test_hand4 = ['8-H', '8-H', '9-H', '9-H', 'JOKER', 'JOKER', 'JOKER', 'JOKER']
+test_hand5 = ['8-S', '8-S', '8-D', '8-D', 'JOKER', 'JOKER']
+first_round_two_sets(test_hand1)
+first_round_two_sets(test_hand2)
+first_round_two_sets(test_hand3)
+first_round_two_sets(test_hand4)
+first_round_two_sets(test_hand5)
+
+# Discard later after figure out logic
+
+# Scenario 1: 2 sets/1 pair/1 JOK ???
+# 4-4, J-J, Q-Q: buy 4 from discard and draw JOK
+# 4-4-4, J-J, Q-Q: JOK, on turn draw J
+# 4-4-4, J-J-J, Q-Q, JOK: best move is LD: J-J-J, Q-Q-JOK
+# (1 set/1 pair/1 JOK > 2 sets/1 JOK).
+
+# Scenario 2: 1 set/2+ pairs/2 JOK ???
+# 4-4, 7-7, J-J, Q-Q, JOK: buy 4 from discard and draw JOK
+# 4-4-4, 7-7, J-J, Q-Q, JOK, JOK: on turn draw 4
+# 4-4-4-4, 7-7, J-J, Q-Q, JOK, JOK:  best move is LD: J-J-JOK, Q-Q-JOK
+# (2 pairs/2 JOK > 1 set/1 pair/2 JOK).
+
